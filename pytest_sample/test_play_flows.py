60905f97909fa367b0d6f0e18aa9a2710b309667
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-07-10 16:23:44 +0300

    draw and player o wins cases

--- pytest_sample/test_play_flows.py
+++ pytest_sample/test_play_flows.py
@@ -19,3 +19,27 @@ class TestPlay:
         self.helper_methods.flow_slots_played(flow)
 
         self.helper_methods.assert_win_info("Player X wins!")
+
+    def test_draw_result(self):
+        self.altdriver.load_scene("StartScene",True)
+
+        play = self.helper_methods.play_button
+        assert play is not None
+        play.click()
+
+        flow = ["8", "5", "9", "7", "3", "6", "4", "1", "2"]
+        self.helper_methods.flow_slots_played(flow)
+
+        self.helper_methods.assert_win_info("It's a draw!")
+    
+    def test_player_O_wins(self):
+        self.altdriver.load_scene("StartScene",True)
+
+        play = self.helper_methods.play_button
+        assert play is not None
+        play.click()
+
+        flow = ["9", "7", "5", "1", "6", "4"]
+        self.helper_methods.flow_slots_played(flow)
+
+        self.helper_methods.assert_win_info("Player O wins!")
