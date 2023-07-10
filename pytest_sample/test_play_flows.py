0a344dbc4c84a1bac8f306f4a538fbfcbc99e526
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-07-10 16:30:54 +0300

    use before and after test  with pytest fixture

--- pytest_sample/test_play_flows.py
+++ pytest_sample/test_play_flows.py
@@ -1,5 +1,6 @@
 from alttester import AltDriver
 from page.helpermethods import HelperMethods
+import pytest
 
 class TestPlay:
 
@@ -8,37 +9,27 @@ class TestPlay:
     def setup_method(self):
         self.helper_methods = HelperMethods(self.altdriver)
 
-    def test_player_X_wins(self):
+    @pytest.fixture(autouse=True)
+    def before_and_after_test(self):
         self.altdriver.load_scene("StartScene",True)
 
         play = self.helper_methods.play_button
         assert play is not None
         play.click()
 
+    def test_player_X_wins(self):
         flow = ["1", "5", "9", "3", "7", "8", "4"]
         self.helper_methods.flow_slots_played(flow)
 
         self.helper_methods.assert_win_info("Player X wins!")
 
     def test_draw_result(self):
-        self.altdriver.load_scene("StartScene",True)
-
-        play = self.helper_methods.play_button
-        assert play is not None
-        play.click()
-
         flow = ["8", "5", "9", "7", "3", "6", "4", "1", "2"]
         self.helper_methods.flow_slots_played(flow)
 
         self.helper_methods.assert_win_info("It's a draw!")
     
     def test_player_O_wins(self):
-        self.altdriver.load_scene("StartScene",True)
-
-        play = self.helper_methods.play_button
-        assert play is not None
-        play.click()
-
         flow = ["9", "7", "5", "1", "6", "4"]
         self.helper_methods.flow_slots_played(flow)
 
